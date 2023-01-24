ec44cdfc2d82b46e8667dfd689f601382fc41631
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-01-24 13:48:04 +0530

    Time: 2 ms (98.99%), Space: 50 MB (74.38%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,3 +1,18 @@
+// class Solution{
+//     public int findJudge(int n, int[][] trust){
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+//         if(n == 1 && trust.length == 0) return 1;
+//         for(int i = 0; i < trust.length; i++){
+//             hm.put(trust[i][0] , trust[i][1]);
+//         }
+//         for(int i = 1; i <= n; i++){
+//             if(!hm.containsKey(i)){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
 class Solution {
     public int findJudge(int n, int[][] trust) {
         if(n==1 && trust.length == 0) return 1;
@@ -15,3 +30,4 @@ class Solution {
         return -1;
     }
 }
+
