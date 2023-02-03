5ade72a8cc2cb07fc3109c3f02c394b307779c73
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-02-04 00:03:27 +0530

    Time: 49 ms (19.95%), Space: 44.4 MB (38.93%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,24 +1,24 @@
 
 class Solution {
     public String convert(String s, int numRows) {
-        int count=0;
-        int row =0;
-        int col=0;
+        int count = 0;
+        int row = 0;
+        int col = 0;
         int pointer = 0;
-        String ans= new String();
-        int n=s.length()%2==0?s.length()/2:(s.length()/2)+1;
+        String ans = new String();
+        int n = s.length()%2 == 0? s.length()/2 : (s.length()/2)+1;
         char[][] arr= new char[numRows][n];
-        if(s.length()==1 || numRows==1){
+        if(s.length() == 1 || numRows == 1){
             return s;
         }
         //for filling . in arr array
-        for(int i=0;i<arr.length;i++){
+        for(int i = 0; i < arr.length; i++){
             for(int j=0;j<arr[i].length;j++){
                 arr[i][j]='#';
             }
         }
         while(true){
-            while(row<numRows&&count < s.length()){
+            while(row < numRows && count < s.length()){
                 arr[row][col] = s.charAt(pointer);
                 pointer++;
                 count++;
@@ -26,22 +26,22 @@ class Solution {
             }
             row=row-2;
             col++;
-          while(row>=0 && count < s.length()){
-              arr[row][col]=s.charAt(pointer);
+          while(row >= 0 && count < s.length()){
+              arr[row][col] = s.charAt(pointer);
               col++;
               row--;
               count++;
               pointer++;
           }
-            row=row+2;
+            row = row + 2;
             col--;
-           if(count==s.length()){
+           if(count == s.length()){
                break;
            } 
         }
-         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                if(arr[i][j]!='#'){
+         for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(arr[i][j] != '#'){
                     ans = ans + arr[i][j];
                 }
             }
