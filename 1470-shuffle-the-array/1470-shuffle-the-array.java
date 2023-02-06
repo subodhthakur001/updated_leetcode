1ab961bba889bfdb423e354a0276c195ad92507b
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-02-07 00:11:48 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (90.42%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,8 +1,10 @@
+// T.C. -> O(logn)
+// S.C. -> O(n)
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int size = nums.length;
         int[] ans = new int[size];
-        int i =0;int j = size / 2;
+        int i =0;int j = n;
         int p = 0;
         while(j < size){
             ans[p++] = nums[i++];
