2156a799657301c5967dee644537843fbc77b299
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-02-20 16:23:17 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (97.57%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        while(s <= e){
+            int mid = (s + e) / 2;
+            if(nums[mid] > target){
+                e = mid - 1;
+            }
+            else if(nums[mid] < target){
+                s = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+      return s;  
+    }
+}
