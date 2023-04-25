322eb0058d0631975952f4c96d32e020464a35a4
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-04-25 23:16:16 +0530

    Time: 3 ms (9.10%), Space: 44.2 MB (37.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<Integer> rows = new ArrayList<>();
-        List<Integer> cols = new ArrayList<>();
+        HashSet<Integer> rows = new HashSet<>();
+        HashSet<Integer> cols = new HashSet<>();
          for(int i = 0;i < matrix.length; i++){
              for(int j = 0; j < matrix[0].length; j++){
                if(matrix[i][j] == 0){
