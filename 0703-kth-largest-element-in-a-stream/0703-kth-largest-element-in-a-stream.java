fc6d524e8e7fdb613c6d1e5d2c411a724c981992
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-05-23 21:49:29 +0530

    Time: 17 ms (53.19%), Space: 46.5 MB (79.68%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (pq.size() < k) {
+            pq.offer(val);
+        } else if (val > pq.peek()) {
+            pq.poll();
+            pq.offer(val);
+        }
+        return pq.peek();
+    }
+}
