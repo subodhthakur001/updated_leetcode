e1419f9fb41dbf1e107d243ef96ed369494d94c3
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-06-01 23:26:59 +0530

    Time: 13 ms (90.25%), Space: 43.9 MB (76.91%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+
+        if (grid[0][0]==1){
+            return -1;
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0,1});
+        grid[0][0]=1;
+        int ct = 0;
+        while(!q.isEmpty()) {
+            int[] cur = q.remove();
+            int i = cur[0];
+            int j = cur[1];
+            int ans = cur[2];
+            if(j==grid[0].length-1 && i==grid.length-1)return ans;
+            for(int x = -1; x <= 1;x ++) {
+                for(int y = -1; y <= 1; y ++){
+                    if(i+x<0 || j+y<0){
+                        continue;
+                    }
+                    if(i+x==grid[0].length || j+y==grid[0].length){
+                        continue;
+                    }
+                    if(grid[i+x][j+y]==0){
+                        q.add(new int[]{i+x,j+y,ans+1});
+                        grid[i+x][j+y]=1;
+                        ct++;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
