f4c604467e7e667b3da27dd8eb1bf01b1defe28f
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-07-12 13:36:08 +0530

    Added solution - LeetHub

--- Power Of Numbers - GFG/power-of-numbers.java
+++ Power Of Numbers - GFG/power-of-numbers.java
@@ -64,7 +64,7 @@ class Solution
        int mod = 1000000007;
        if(N == 0) return 0;
        if(R == 0) return 1;
-       if(R == 1) return N;
+    //   if(R == 1) return N;
        if(R%2 == 0){
            long ans = power(N,R/2);
            return ((ans % mod)* (ans %mod))%mod;
