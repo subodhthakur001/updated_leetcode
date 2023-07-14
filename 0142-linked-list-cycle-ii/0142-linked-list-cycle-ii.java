313d04ca89f7661fc179f2e4b135ac4d21b3ec02
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-07-15 03:35:39 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (39.33%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -13,22 +13,21 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode s = head;
         ListNode f = head;
-        while(f != null && f.next!= null){
+        while(f != null && f.next != null){
             s = s.next;
             f = f.next.next;
-            if( s == f){
+            if(s == f){
                 break;
-            }
+            } 
         }
-        if(f == null || f.next == null){
+        if(f == null || f.next == null) {
             return null;
         }
         s = head;
         while(s != f){
             s = s.next;
-            f = f.next;
+            f = f.next;   
         }
-        
         return s;
     }
 }
