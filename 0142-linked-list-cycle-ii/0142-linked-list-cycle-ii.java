61d2f5ed74a5bae319ffb472a7d9c7e39970ca8b
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-07-15 03:36:18 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (55.23%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -17,13 +17,14 @@ public class Solution {
             s = s.next;
             f = f.next.next;
             if(s == f){
+                s = head;
                 break;
             } 
         }
         if(f == null || f.next == null) {
             return null;
         }
-        s = head;
+        // s = head;
         while(s != f){
             s = s.next;
             f = f.next;   
