d7033a470ae4d36af7da14a39c78d7a4ae8e2e58
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2023-07-27 23:24:43 +0530

    Time: 2 ms (91.68%), Space: 40.7 MB (32.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i =0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
-                stack.push(s.charAt(i));
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push(s.charAt(i));
             }
-            else if(s.charAt(i)==')'){
-                if(!stack.isEmpty() && stack.peek()=='('){
-                    stack.pop();
+            else if(s.charAt(i) == '{'){
+                st.push(s.charAt(i));
+            }
+            else if(s.charAt(i) == '['){
+                st.push(s.charAt(i));
+            }
+            else if(s.charAt(i) == ')'){
+                  if(!st.isEmpty() && st.peek() == '('){
+                      st.pop();
+                  }
+                else return false;
+            }
+            else if(s.charAt(i) == '}'){
+                if(!st.isEmpty() && st.peek() == '{'){
+                    st.pop();
                 }
                 else return false;
             }
-            else if(s.charAt(i)=='}'){
-                if(!stack.isEmpty() && stack.peek()=='{'){
-                    stack.pop();
-                }
-                else return false;
-                }
-            else if(s.charAt(i)==']'){
-                if(!stack.isEmpty() && stack.peek()=='['){
-                    stack.pop();
+            else if(s.charAt(i) == ']'){
+                if(!st.isEmpty() && st.peek() == '['){
+                    st.pop();
                 }
                 else return false;
             }
-                
-            }
-        if(stack.isEmpty() ==true) return true;
-        else return false;
-        }
-            
-  }
-    
+    }
+        return st.isEmpty();
+}
+}
