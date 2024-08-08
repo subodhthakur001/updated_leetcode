e8e5aab5bd9ebd991500e4a9a9e9a274a2079b06
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2024-08-08 17:01:51 +0530

    Time: 4 ms (86.38%), Space: 60.8 MB (62.74%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,33 +9,34 @@
  * }
  */
 class Solution {
-       public ListNode reverse(ListNode s){
-           ListNode curr = s;
-           ListNode prev = null;
-           ListNode temp = curr;
-           while(temp != null){
-               temp = temp.next;
-               curr.next = prev;
-               prev = curr;
-               curr = temp;
-           }
-           return prev;
-       }
+    public ListNode reverseList(ListNode s){
+        ListNode curr = s;
+        ListNode temp = curr;
+        ListNode prev = null;
+        while(temp != null){
+            temp = temp.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        ListNode s = head;
-        ListNode f = head;
-        while(f.next != null && f.next.next != null){
-            s = s.next;
-            f = f.next.next;
-        }
-         s.next = reverse(s.next);
-         s = s.next;
-        f  = head;
-        while(s != null){
-            if(s.val != f.val) return false;
-            s = s.next;
-            f = f.next;
-        }
-        return true;
+       ListNode s = head;
+       ListNode f = head;
+       
+       while(f.next != null && f.next.next != null){
+           s = s.next;
+           f = f.next.next;
+       }
+       s.next = reverseList(s.next);
+       s = s.next;
+       f = head;
+       while(s != null){
+           if(s.val != f.val) return false;
+           s = s.next;
+           f = f.next;
+       }
+    return true;
     }
 }
